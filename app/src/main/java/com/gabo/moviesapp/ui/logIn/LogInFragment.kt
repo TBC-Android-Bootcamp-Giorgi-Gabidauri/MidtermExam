3c208e62fe30b041bf96966c9edf7176328cb648
Luka Mazmishvili <luka.mazmishvili.2@btu.edu.ge>
2022-08-27 16:54:41 +0400

    Authentication done and bug fixed

--- app/src/main/java/com/gabo/moviesapp/ui/logIn/LogInFragment.kt
+++ app/src/main/java/com/gabo/moviesapp/ui/logIn/LogInFragment.kt
@@ -1,22 +1,57 @@
 package com.gabo.moviesapp.ui.logIn
 
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.gabo.moviesapp.other.base.BaseFragment
 import com.gabo.moviesapp.databinding.FragmentLogInBinding
+import com.gabo.moviesapp.other.base.BaseFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LogInFragment : BaseFragment<LogInViewModel, FragmentLogInBinding>(
     LogInViewModel::class,
     FragmentLogInBinding::inflate
 ) {
     override fun setupView(savedInstanceState: Bundle?) {
-//        binding.tv.setOnClickListener {
-//            findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToViewPagerContainerFragment())
-//        }
+        checkSession()
+        listeners()
+    }
+
+    private fun checkSession(){
+        FirebaseAuth.getInstance().currentUser?.uid?.let {
+            findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToViewPagerContainerFragment())
+        }
+    }
+
+    private fun listeners(){
+        binding.btnLogin.setOnClickListener {
+            login(
+                binding.etEmail.text.toString().trim(),
+                binding.etPassword.text.toString().trim()
+            )
+        }
 
         binding.signUpLink.setOnClickListener {
             findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToRegisterFragment())
         }
     }
 
+    private fun login(email: String, password: String) {
+        if (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches() && password.isNotEmpty() && password.length > 6
+        ) {
+            Firebase.auth.signInWithEmailAndPassword(
+                email,
+                password
+            ).addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(requireContext(), "Logged in successfully", Toast.LENGTH_SHORT)
+                        .show()
+                    findNavController().navigate(LogInFragmentDirections.actionLogInFragmentToViewPagerContainerFragment())
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/gabo/moviesapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/gabo/moviesapp/ui/register/RegisterFragment.kt
@@ -1,14 +1,40 @@
 package com.gabo.moviesapp.ui.register
 
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.gabo.moviesapp.other.base.BaseFragment
 import com.gabo.moviesapp.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : BaseFragment<RegisterViewModel, FragmentRegisterBinding>(
     RegisterViewModel::class,
     FragmentRegisterBinding::inflate
 ) {
     override fun setupView(savedInstanceState: Bundle?) {
+
+        binding.btnRegister.setOnClickListener {
+            register(
+                binding.etEmail.text.toString().trim(),
+                binding.etPassword.text.toString().trim()
+            )
+        }
+    }
+
+    private fun register(email: String, password: String) {
+        if (email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches() && password.isNotEmpty() && password.length > 6
+        ) {
+            Firebase.auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(requireContext(), "Successfully Registered", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(requireContext(), "error!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     }
 }
